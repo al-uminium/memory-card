aacd35ba4c0be8415b382aec084633bba472eb4c
al-uminium <aeronafic@gmail.com>
2022-12-27 13:26:33 +0800

    fixed typo in file name

---
+++ src/components/StageBoard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StageBoards = () => {
+    return (
+        <div className='StageBoard'>
+            Stage 1
+        </div>
+    );
+}
+
+export default StageBoards;
